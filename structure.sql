8f8e9e7b24946ba72d01df6dc73f764deb202dc9
Robert Ross <rob@Robs-MacBook-Pro-13.local>
2019-06-08 19:03:57 -0700

    initial push

---
+++ structure.sql
@@ -0,0 +1,13 @@
+use ;
+CREATE TABLE top_albums (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  album VARCHAR(100) NULL,
+  releaseYear INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
+  raw_eur DECIMAL(10,4) NULL,
+  raw_row DECIMAL(10,4) NULL,
+  PRIMARY KEY (position)
+);
