e7f127207071214d4caa2bed02c23bd0d5041644
Robert Ross <rob@Robs-MacBook-Pro-13.local>
2019-06-09 13:41:34 -0700

    working code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -29,10 +29,13 @@ function resetVars() {
   var requested = 0;
   var onHand = 0
   var itemName = "";
-  start();
-  };
+  recID = 0;
+  totalPrice = 0;
+};
+start();
 // function which prompts the user for what action they should take
 function start() {
+  console.log("\033c");
   connection.query("SELECT * FROM products", function (err, results) {
     if (err) throw err;
     inquirer
@@ -65,8 +68,10 @@ function start() {
             requested = answer.units;
             onHand = results[i].stock_qty;
             itemName = results[i].product_name;
-            console.log("Amount Requested to Buy: " + requested);
-            console.log("Amount on Hand: " + onHand);
+            recID = i;
+            totalPrice = results[i].price;
+            console.log("\nAmount Requested to Buy: " + requested + "\n");
+            console.log("Amount on Hand: " + onHand + "\n");
             checkStock();
           }
         }
@@ -75,16 +80,37 @@ function start() {
 }
 function checkStock() {
   if (onHand < requested) {
-    console.log("I'm sorry but the maximum amount we have in stock is " + onHand);
+    console.log("I'm sorry but the maximum amount we have in stock is " + onHand + ".\n\nI will now restart the application in 5 seconds, please wait." + "\n");
+    setTimeout (start, 5000);
+    return;
   } else {
     decreaseStock();
-  return;
+    return;
   }
 }
 function decreaseStock() {
-  connection.query("SELECT * FROM products", function (err, results) {
-    if (err) throw err;
-    console.log("I have logged your order of quantity " + requested + " of item " + itemName);
-    console.log("This leaves the store with a remaining balance of " + (onHand - requested));
-});
-}
+  recID++;
+  var newStockQty = (onHand - requested);
+    connection.query("UPDATE bamazon.products SET ? WHERE ?",
+    [
+      {
+        stock_qty: newStockQty
+      },
+      {
+        item_id: recID
+      },
+    ],
+      function (err) {
+        if (err) throw err;
+        // output to the 
+        console.log("I have logged your order of quantity " + requested + " of item " + itemName + "\n");
+        console.log("This leaves the store with a remaining balance of " + (onHand - requested) + "\n");
+        console.log("The total cost of this purchase is: $" + Math.round((totalPrice * requested) * 100) / 100 + "\n")
+        console.log("Let's not forget the governments portion of sales tax: $" + Math.round(((totalPrice * requested) * .0775) * 100) / 100 + "\n")
+        console.log("Your new grand total for this purchase is $" + Math.round(((totalPrice * requested) * 1.0775) * 100) / 100 + "\n")
+        console.log("please stand by and I will restart the app in 10 seconds.\n")
+        setTimeout (start, 7000);
+    return;
+      }
+  );
+};
